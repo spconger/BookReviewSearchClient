77da9021ba9d2753fdcd6584aa958c394980e94e
Steve Conger <spconger@gmail.com>
2015-12-12 09:28:29 -0800

    addprogramfiles
    files for the client to the Book Review select service

---
+++ Default.aspx
@@ -0,0 +1,31 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    <h1>Choose a Review by Title</h1>
+        <p><asp:DropDownList ID="AuthorDropDownList" runat="server"></asp:DropDownList><br />
+            <asp:Button ID="ReviewsButton" runat="server" Text="Get Reviews" OnClick="ReviewsButton_Click" />
+            <asp:DataList ID="DataList1" runat="server">
+                <ItemTemplate>
+                    <h2><%#Eval("BookTitle") %></h2>
+                    <hr />
+                    <h3><%#Eval("ReviewTitle") %></h3>
+                    <p><strong><%#Eval("ReviewerName") %></strong></p>
+                    <p><%#Eval("ReviewDate") %>    rating  <%#Eval("BookRating") %></p>
+                    <hr />
+                    <p><%#Eval("ReviewBody") %></p>
+
+                </ItemTemplate>
+
+            </asp:DataList>
+    </div>
+    </form>
+</body>
+</html>
---
+++ Default.aspx.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    SelectServiceReference.SelectServiceClient sc = 
+        new SelectServiceReference.SelectServiceClient();
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!IsPostBack)
+        {
+            PopulateAuthorDropDownList();
+        }
+
+    }   
+
+    protected void ReviewsButton_Click(object sender, EventArgs e)
+    {
+        PopulateDataList();
+
+    }
+
+    protected void PopulateAuthorDropDownList()
+    {
+        string[] authors = sc.GetAuthors();
+        AuthorDropDownList.DataSource = authors;
+        AuthorDropDownList.DataBind();
+    }
+
+    protected void PopulateDataList()
+    {
+        string authorName = AuthorDropDownList.SelectedItem.ToString();
+        SelectServiceReference.ReviewInfo[] reviews = sc.GetReviewsByAuthor(authorName);
+        DataList1.DataSource = reviews;
+        DataList1.DataBind();
+
+    }
+
+
+}
